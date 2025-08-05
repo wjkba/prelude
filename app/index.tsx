c59a012b4eed29d64ebd0ba3d1fe175177db70a3
wjkba <63542318+wjkba@users.noreply.github.com>
2025-08-05 11:37:16 +0200

    add tailwind config

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="bg-purple-500 items-center justify-center flex-1">
+      <Text>prelud</Text>
+    </View>
+  );
+}
